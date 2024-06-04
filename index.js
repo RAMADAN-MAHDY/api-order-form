ffd0ec9d825b082078b4b841aac250f7b23937ca
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-06-04 23:55:17 +0300

    Updated data2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 5000;
 const server = createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:'http://localhost:3000'
+        origin:'https://royal-corner.vercel.app'
     }
 });
 
